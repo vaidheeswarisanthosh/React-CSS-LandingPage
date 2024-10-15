8bfe0cdc669aa4fb97edcaee281b14ca1317e387
vaidheeswarisanthosh <vaidheeswarivm@gmail.com>
2024-10-15 10:45:52 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Landingpage from './components/Landingpage'
+
+
+
+
+  const App=()=>{
+
+  return (
+   
+      <div>
+       
+        <Landingpage/>
+
+      </div>
+      
+      
+  
+  )
+}
+
+export default App;
